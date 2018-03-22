4617bdd44cc6feb429e95580e2a5e62f72b13e15
Tobias <tobiaselinder@gmail.com>
2018-03-22 15:41:51 +0100

    improvements

--- Renderer.hs
+++ Renderer.hs
@@ -4,8 +4,8 @@ import Data.Set
 import Control.Concurrent
 import Data.IORef
 
-dist = 0.025
-scaleS = 0.05
+dist = 0.0045
+scaleS = 0.01
 both f (x,y) = (f x, f y)
 scalePoint = both (*scaleS)
 getBox (x,y) = [(x-dist, y-dist, 0),(x + dist, y - dist, 0.0), (x+dist, y + dist, 0), (x-dist,y + dist, 0)]
@@ -17,19 +17,25 @@ main :: IO ()
 main =  do
   (_progName, _args) <- getArgsAndInitialize
   i <- newIORef 0       -- new IORef i
+  test <- newIORef [(1,2)]
   _window <- createWindow "LIFE"
-  --displayCallback $= (display i)
+  reshapeCallback $= Just reshape
+  displayCallback $= display i  
   idleCallback $= Just (display i)
   mainLoop
  
 display m = do 
   readIORef m >>= print -- print it
   clear [ColorBuffer]
-  modifyIORef m (+1)    -- increase it by 1
   generation <- readIORef m
   renderPrimitive Quads  $
      mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) $ lifeToPoints generation
-  threadDelay 100000
+  modifyIORef m (+1)    -- increase it by 1
+  --threadDelay 100000
   flush
 
+reshape :: ReshapeCallback
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
 
